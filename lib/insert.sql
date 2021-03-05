b86b5838f33ecb59f4be8160ac27c0768850e802
Ian Davies <mr.daviesian@gmail.com>
2021-03-05 09:41:01 -0600

    commit tree 98c5e23e2f8e03fe39a03bc26a913166120dd637

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 52, 'male', 'brown','easy going', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'brown','easy going', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'female', 'brown','easy going', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 40, 'male', 'brown','easy going', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 16, 'female', 'brown','easy going', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 73, 'male', 'brown','easy going', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 23, 'female', 'brown','easy going', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 52, 'male', 'brown','easy going', false);
